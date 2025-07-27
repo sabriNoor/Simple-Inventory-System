37b0325dcf28436043c447397b22c810863f6290
sabriNoor <nooraosama63@gmail.com>
2025-07-27 14:29:25 +0300

    style: make Program class public and add error handling to Main method

--- Program.cs
+++ Program.cs
@@ -1,14 +1,26 @@
 ﻿using SimpleInventorySystem.Services;
 using SimpleInventorySystem.Models.Interfaces;
 using SimpleInventorySystem.Views;
-class Program
+using SimpleInventorySystem.Utils;
+public class Program
 {
     public static void Main()
     {
-        IInventoryOperations inventoryOperations = new Operations();
-        IInventoryOperationsView inventoryOperationsView = new OperationsView(inventoryOperations);
-        IInventoryMenuView menuView = new MenuView(inventoryOperationsView);
-        menuView.ExecuteMenu();
+        try
+        {
+            Logger.LogInfo("Application started.");
+            IInventoryOperations inventoryOperations = new Operations();
+            IInventoryOperationsView inventoryOperationsView = new OperationsView(inventoryOperations);
+            IInventoryMenuView menuView = new MenuView(inventoryOperationsView);
+            menuView.ExecuteMenu();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error initializing logger: {ex.Message}");
+            Logger.LogError($"Error initializing logger: {ex.Message}");
+            return;
+        }
 
     }
 
