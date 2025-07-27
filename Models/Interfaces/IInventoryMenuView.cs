67b46968f522bec8eb4476c5c95c94daa4ca59f4
sabriNoor <nooraosama63@gmail.com>
2025-07-27 14:08:20 +0300

    docs(interface): add XML documentation to IInventoryMenuView

--- Models/Interfaces/IInventoryMenuView.cs
+++ Models/Interfaces/IInventoryMenuView.cs
@@ -1,4 +1,8 @@
 namespace SimpleInventorySystem.Models.Interfaces;
+/// <summary>
+/// Interface for the inventory menu view.
+/// This interface defines the contract for displaying and executing the inventory menu.
+/// </summary>
 interface IInventoryMenuView
 {
     void ExecuteMenu();
